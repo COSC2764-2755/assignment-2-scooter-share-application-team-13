a37e1767f8d9967757188cb353ff3b19eb6dba4b
Lachlan Stewart-Pearce <s3845132@student.rmit.edu.au>
2023-09-20 14:18:29 +1000

    Define customer class

--- ScootShare/records.py
+++ ScootShare/records.py
@@ -1,1 +1,13 @@
 # Store classes
+
+class Customer():
+    def __init__(self, id, f_name, l_name, ph_num, email, username, password, balance) -> None:
+        self._customer_id = id
+        self._first_name = f_name
+        self._last_name = l_name
+        self._phone_number = ph_num
+        self._email_address = email
+        self._username = username
+        self._password = password
+        self._balance = balance
+
