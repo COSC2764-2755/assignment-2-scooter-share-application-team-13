e977547bfc9441416dae6ed20911ef86a7a56790
Lachlan Stewart-Pearce <s3845132@student.rmit.edu.au>
2023-09-20 14:48:43 +1000

    Update customer attributes to public

--- ScootShare/records.py
+++ ScootShare/records.py
@@ -2,12 +2,12 @@
 
 class Customer():
     def __init__(self, id, f_name, l_name, ph_num, email, username, password, balance) -> None:
-        self._customer_id = id
-        self._first_name = f_name
-        self._last_name = l_name
-        self._phone_number = ph_num
-        self._email_address = email
-        self._username = username
-        self._password = password
-        self._balance = balance
+        self.customer_id = id
+        self.first_name = f_name
+        self.last_name = l_name
+        self.phone_number = ph_num
+        self.email_address = email
+        self.username = username
+        self.password = password
+        self.balance = balance
 
