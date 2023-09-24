0542c733150636ca0e43198d1ec506390644f617
Lachlan Stewart-Pearce <s3845132@student.rmit.edu.au>
2023-09-24 10:48:41 +1000

    Update endpoints to POST from PUT

--- ScootShare/app.py
+++ ScootShare/app.py
@@ -11,18 +11,18 @@ api = Api(app)
 class Registration(Resource):
     def __init__(self) -> None:
         super().__init__()
-        self._cust_put_args = reqparse.RequestParser()
-        self._cust_put_args.add_argument("id", type=int, help="CustomerID")
-        self._cust_put_args.add_argument("first_name", type=str, help="Customer fName")
-        self._cust_put_args.add_argument("last_name", type=str, help="Customer lName")
-        self._cust_put_args.add_argument("phone_number", type=str, help="phone num")
-        self._cust_put_args.add_argument("email_address", type=str, help="email")
-        self._cust_put_args.add_argument("username", type=str, help="username")
-        self._cust_put_args.add_argument("password", type=str, help="password")
-        self._cust_put_args.add_argument("balance", type=float, help="balance")
+        self._cust_reg_args = reqparse.RequestParser()
+        self._cust_reg_args.add_argument("id", type=int, help="CustomerID")
+        self._cust_reg_args.add_argument("first_name", type=str, help="Customer fName")
+        self._cust_reg_args.add_argument("last_name", type=str, help="Customer lName")
+        self._cust_reg_args.add_argument("phone_number", type=str, help="phone num")
+        self._cust_reg_args.add_argument("email_address", type=str, help="email")
+        self._cust_reg_args.add_argument("username", type=str, help="username")
+        self._cust_reg_args.add_argument("password", type=str, help="password")
+        self._cust_reg_args.add_argument("balance", type=float, help="balance")
 
-    def put(self):
-        args = self._cust_put_args.parse_args()
+    def post(self):
+        args = self._cust_reg_args.parse_args()
         customer_object = Customer(args['id'], args['first_name'], 
                                    args['last_name'], args['phone_number'], 
                                    args['email_address'], args['username'], 
