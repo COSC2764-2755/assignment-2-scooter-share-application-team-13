63aefe29ae0789473cc6a939d42da57f05f6e18e
Lachlan Stewart-Pearce <s3845132@student.rmit.edu.au>
2023-09-24 10:38:57 +1000

    Remove customer ID - Username is primary key now

--- ScootShare/db.py
+++ ScootShare/db.py
@@ -6,34 +6,32 @@ class DatabaseConnector:
     def __init__(self) -> None:
         self._file = '.\ScootShare\ScootShare.db'
 
-    def create_table(self):
+    def create_table(self) -> None:
         con = lite.connect(self._file)
         with con: 
             cur = con.cursor() 
             cur.execute("DROP TABLE IF EXISTS Customer") # Temporary while db is local
-            cur.execute("CREATE TABLE Customer (    \
-                        id INT PRIMARY KEY,         \
-                        first_name VARCHAR(255),    \
-                        last_name VARCHAR(255),     \
-                        phone_number VARCHAR(20),   \
-                        email_address VARCHAR(255), \
-                        username VARCHAR(50),       \
-                        password VARCHAR(255),      \
+            cur.execute("CREATE TABLE Customer (            \
+                        username VARCHAR(50) PRIMARY KEY,   \
+                        first_name VARCHAR(255),            \
+                        last_name VARCHAR(255),             \
+                        phone_number VARCHAR(20),           \
+                        email_address VARCHAR(255),         \
+                        password VARCHAR(255),              \
                         balance DECIMAL(10, 2));"
                         )
             con.commit()
     
-    def add_customer(self, new_customer:Customer):
+    def add_customer(self, new_customer:Customer) -> None:
         con = lite.connect(self._file)
         with con: 
             cur = con.cursor() 
-            query = "INSERT INTO Customer (id, first_name, last_name, phone_number, email_address, username, password, balance) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
-            customer_data = (new_customer.customer_id,
+            query = "INSERT INTO Customer (username, first_name, last_name, phone_number, email_address, password, balance) VALUES (?, ?, ?, ?, ?, ?, ?)"
+            customer_data = (new_customer.username, # username is primary key
                              new_customer.first_name,
                              new_customer.last_name,
                              new_customer.phone_number,
                              new_customer.email_address,
-                             new_customer.username,
                              hash_password(new_customer.password),
                              new_customer.balance)
             cur.execute(query, customer_data)
