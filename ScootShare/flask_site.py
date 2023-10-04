60982ead5ea0d867b311b7583882da3432cb07b1
lahiru99 <lahiru7675@gmail.com>
2023-10-04 06:44:04 +0100

    added dashboard and report_issue

--- ScootShare/flask_site.py
+++ ScootShare/flask_site.py
@@ -27,3 +27,16 @@ def booking_view():
 def dashboard_view():
     return render_template("dashboard.html")
 
+@site.route('/report_issue')
+def report_issue():
+    return render_template('report_issue.html')
+
+@site.route('/submit_issue', methods=['POST'])
+def submit_issue():
+   # scooter_id = request.form['scooter_id']
+   # issue_description = request.form['issue_description']
+    
+    # Save the reported issue to your database or handle as needed
+    # ...
+
+    return redirect(url_for('dashboard'))  # Redirect back to the dashboard after submitting
