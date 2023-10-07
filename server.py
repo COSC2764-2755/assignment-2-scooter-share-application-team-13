439b31b824db5d88d0b2dc81aa047f9e1e6aeaca
s3838911 <s3838911@student.rmit.edu.au>
2023-10-07 16:23:01 +1100

    further work

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import socket
+import requests, json
 from passlib.hash import sha256_crypt
 
-
+BASE = "http://127.0.0.1:5000/api/" #
 USERNAME = "admin"
 PASSWORD_HASH = sha256_crypt.hash("pass123")
 HOST = ""
@@ -11,40 +12,45 @@ ADDRESS = (HOST, PORT)
 print("Server Initializing...")
 print("Server Ready. Awaiting connections.")
 
+
+def validate_login(received_username, received_hash):
+    endpoint = "login" #We need to ensure this matches the function
+    response = request.ge
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(ADDRESS)
     s.listen()
 
-    while True: 
-        print("Listening on {}...".format(ADDRESS))
+    while True:
+        print(f"Listening on {ADDRESS}...")
         conn, addr = s.accept()
         with conn:
             IP = addr[0]
             socket_addr = addr[1]
-            print(f"IP address: {IP} Socket address {socket_addr}")
-          
+            print(f"IP address: {IP} Socket address: {socket_addr}")
+
             # Receive username and hashed password from client
             data = conn.recv(4096).decode()
             received_username, received_hash = data.split(":")
 
-            print(f"IP address: {received_username} Something else {received_hash}")
+            print(f"Received username: {received_username} Received hash: {received_hash}")
 
+            # Now use API to make a customer object based on supplied username (received_username)
 
-        USERNAME = 'Joshua'
-
-            # Checking the received username and hash match the stored values
-            if received_username == USERNAME and sha256_crypt.verify("pass123", received_hash):
+            # Checking if the received username and hash match the stored values
+            if sha256_crypt.verify("pass123", received_hash):
                 conn.sendall("Authentication Successful!".encode())
-                print("Authentication Successful for Scooter [{}].".format(scooter_id))
+                print(f"Authentication Successful for user: [{received_username}].")
             else:
                 conn.sendall("Authentication Failed!".encode())
-                print("Authentication Failed for Scooter [{}].".format(scooter_id))
+                print(f"Authentication Failed for user: [{received_username}].")
 
-            print("Scooter [{}] disconnected.".format(scooter_id))
 
-        print("Awaiting next scooter connection...")
 
-    print("Server shutting down...")
+
+
+      
 
 
 
