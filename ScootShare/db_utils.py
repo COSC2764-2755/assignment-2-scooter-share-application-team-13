c408c8322d903641529399e25ad574d12ba23d06
Lachlan Stewart-Pearce <s3845132@student.rmit.edu.au>
2023-10-08 13:11:24 +1100

    Remove debug password print

--- ScootShare/db_utils.py
+++ ScootShare/db_utils.py
@@ -5,7 +5,6 @@ def hash_password(password: str) -> str:
     # Generate a random salt
     salt = bcrypt.gensalt()
     if password is not None:
-        print(password)
         # Hash the password with the salt
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
         # Return the hashed password as bytes
